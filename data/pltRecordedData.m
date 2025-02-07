dcba88307fef979c7c10cf408b6441051f567b99
nahueldeniz <ndeniz@sinc.unl.edu.ar>
2025-02-07 17:56:44 -0300

    Add *.mat files and folders with more *.mat files.

---
+++ data/pltRecordedData.m
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:df366b6ee80535f3cfe5cea3fb416a94a81280e3def79f7fc391d04483fa00b2
+size 2015
---
+++ data/test_vectornav.m
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2dd38b3fa75ae2b84e7d5ee1055d84c9a685ffd3e86bf6a78f7fb981fbfd0bae
+size 9460
