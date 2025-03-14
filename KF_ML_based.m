5fd6ad52393a5c74f47a52200de06a402300c0c6
nahueldeniz <ndeniz@sinc.unl.edu.ar>
2025-03-14 11:28:28 -0300

    Added some paper about ML-based estimation and started implementing one of them.

---
+++ KF_ML_based.m
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c147223f2f26d7f99ebe5a956263122bd6610922b43f05910423fbb965a60919
+size 3153
---
+++ RAL_revisions/algorithms_to_compare/OIKF_NUV.m
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:209b16e76e1f8c3b61d2fc7841b52b0df19d32cb14dad80e79399972e5788241
+size 15931
